1a281d7c9ceb1ca69b55cfa4ca9210d138340a16
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2022-07-06 19:07:54 -0400

    linked api results to 5day forecast cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,11 @@
 var city;
 var asideSection = document.getElementById("clickListener");
 var date = moment().format('L');
-
+ 
+for (var j = 0; j < 5; j++){
+    var forecastDates = document.getElementById("container" + j);
+    // forecastDates.textContent = moment().add(j+1, 'days').calendar(); 
+}
 
 var latLon = {
     APIKey: "a837c12832ce1b1f143bd6108aa84e92",
@@ -32,17 +36,6 @@ var latLon = {
 
 }
 
-
-// var location = this.getLocation;
-// fetch(
-//     "https://api.openweathermap.org/data/2.5/onecall?lat="
-//     + lat
-//     + "&lon="
-//     + lon
-//     + "&appid="
-//     + this.APIKey
-// )
-
 var weather = {
     APIKey: "a837c12832ce1b1f143bd6108aa84e92",
     
@@ -56,11 +49,6 @@ var weather = {
             + "&units=imperial"
             + "&appid="
             + this.APIKey
-            // "http://api.openweathermap.org/data/2.5/weather?q="
-            // + city
-            // + "&units=imperial"
-            // + "&appid="
-            // + this.APIKey
         )
         .then((response) => {
             return response.json();
@@ -69,16 +57,44 @@ var weather = {
       },
     displayWeather: function(data) {
         console.log(data);
-        // const { name } = data;
-        // const { temp, humidity} = data.main;
         const { temp, humidity} = data.current;
         const { uvi } = data.current;
-        // const { speed } = data.wind;
         const { wind_speed } = data.current;
-        document.querySelector(".temp").innerText = "Temp: " + temp + "°F";
+        const { icon, description } = data.current.weather[0];
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = "Temp: " + temp + " °F";
         document.querySelector(".wind").innerText = "Wind: " + wind_speed + " MPH";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".uv").innerText = "UV Index: " + uvi;
+        document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+
+
+        console.log(data.daily[0].weather);
+        for (var k = 0; k < 5; k++){
+            //Time of the forecasted data, Unix, UTC
+            const { dt } = data.daily[k];
+            //Temperature
+            const { day } = data.daily[k].temp;
+            const {wind_speed} = data.daily[k];
+            const { humidity } = data.daily[k];
+            const { icon } = data.daily[k].weather[0];
+
+            //converting date to regular format
+            // const milliseconds = dt * 1000;
+            // const dateObject = new Date(milliseconds);
+            // const humanDateFormat = dateObject.toLocaleString(); 
+            // console.log(humanDateFormat);
+
+            document.getElementById("forecastDate" + [k]).innerText = moment.unix(dt).format("MM/DD/YYYY");;
+            document.getElementById("temp" + [k]).innerText = "Temp: " + day + " °F";
+            document.getElementById("wind" + [k]).innerText = "Wind: " + wind_speed + " MPH";
+            document.getElementById("hmidity" + [k]).innerText = "Humidity: " + humidity + "%";
+            document.getElementById("icon" + [k]).src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+
+        }
+
     }
 };
 
@@ -96,28 +112,6 @@ asideSection.addEventListener('click', function(event){
 
     latLon.fetchLocation();
 
-    //var locInfo = latLon.fetchLocation();
-    /*var latLonResult;
-
-    console.log(locInfo);
-
-    locInfo.then((data) => weather.fetchWeather(data));*/
-   
-    /*const locInfoResult = async () => {
-        const a = await locInfo;
-        weather.fetchWeather(a);
-
-        //console.log(a);
-      };    
-    locInfoResult();*/
-
-    //latLonResult = locInfoResult();
-    
-    //locInfo.then((data) => weather.fetchWeather(data));
-    //weather.fetchWeather();
-
-    
-    //console.log(latLonResult);
 });
 
 
