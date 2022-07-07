37ea210d52585865656c92c7ef651b56e120e397
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2022-07-06 21:06:43 -0400

    added screenshot to readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Weather-Dashboard
+
+* This app will show you current weather and and 5 day forecase for the cuty you selected.
+
+## Links to the app and HitHub
+
+*
+
+*
+
+![screenshot](https://sun9-70.userapi.com/impf/J8LlxUR8CDquJBksNC4koKD8qVDOIDPHC1HGIw/Ts8oVyjkL-Q.jpg?size=1906x617&quality=95&sign=e9fc31c794d340368d633469ec399da7&type=album)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 var city;
 var asideSection = document.getElementById("clickListener");
 var date = moment().format('L');
- 
+
 // var history0 = document.getElementById("btn0");
 // var history1 = document.getElementById("btn1");
 // var history2 = document.getElementById("btn2");
@@ -11,15 +11,15 @@ var date = moment().format('L');
 // var history5 = document.getElementById("btn5");
 // var history6 = document.getElementById("btn6");
 
-for (var y = 0; y < 7; y++){
-    var historybtn = document.getElementById("btn"+y);
-    historybtn.textContent = localStorage.getItem("History"+y);
+for (var y = 0; y < 7; y++) {
+    var historybtn = document.getElementById("btn" + y);
+    historybtn.textContent = localStorage.getItem("History" + y);
 }
 
 var latLon = {
     APIKey: "a837c12832ce1b1f143bd6108aa84e92",
 
-    fetchLocation: function() {
+    fetchLocation: function () {
         fetch(
             "http://api.openweathermap.org/geo/1.0/direct?q="
             + city
@@ -27,27 +27,27 @@ var latLon = {
             + this.APIKey
         ).then((response) => {
             return response.json();
-          })
-          .then((data) => this.getLocation(data))
+        })
+            .then((data) => this.getLocation(data))
             .then((data) => {
                 weather.fetchWeather(data);
             });
 
     },
-    getLocation: function(data) {
+    getLocation: function (data) {
 
         var lat = data[0].lat;
         var lon = data[0].lon;
-        
-        return { lat, lon };   
+
+        return { lat, lon };
     },
 
 }
 
 var weather = {
     APIKey: "a837c12832ce1b1f143bd6108aa84e92",
-    
-    fetchWeather: function(a) {
+
+    fetchWeather: function (a) {
 
         fetch(
             "https://api.openweathermap.org/data/2.5/onecall?lat="
@@ -58,14 +58,14 @@ var weather = {
             + "&appid="
             + this.APIKey
         )
-        .then((response) => {
-            return response.json();
-          })
-          .then((data) => this.displayWeather(data));
-      },
-    displayWeather: function(data) {
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
         console.log(data);
-        const { temp, humidity} = data.current;
+        const { temp, humidity } = data.current;
         const { uvi } = data.current;
         const { wind_speed } = data.current;
         const { icon, description } = data.current.weather[0];
@@ -75,16 +75,16 @@ var weather = {
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".uv").innerText = "UV Index: " + uvi;
         document.querySelector(".icon").src =
-        "https://openweathermap.org/img/wn/" + icon + ".png";
+            "https://openweathermap.org/img/wn/" + icon + ".png";
 
 
         console.log(data.daily[0].weather);
-        for (var k = 0; k < 5; k++){
+        for (var k = 0; k < 5; k++) {
             //Time of the forecasted data, Unix, UTC
             const { dt } = data.daily[k];
             //Temperature
             const { day } = data.daily[k].temp;
-            const {wind_speed} = data.daily[k];
+            const { wind_speed } = data.daily[k];
             const { humidity } = data.daily[k];
             const { icon } = data.daily[k].weather[0];
 
@@ -99,7 +99,7 @@ var weather = {
             document.getElementById("wind" + [k]).innerText = "Wind: " + wind_speed + " MPH";
             document.getElementById("hmidity" + [k]).innerText = "Humidity: " + humidity + "%";
             document.getElementById("icon" + [k]).src =
-            "https://openweathermap.org/img/wn/" + icon + ".png";
+                "https://openweathermap.org/img/wn/" + icon + ".png";
 
         }
 
@@ -107,22 +107,22 @@ var weather = {
 };
 
 var i = 0;
-asideSection.addEventListener('click', function(event){
+asideSection.addEventListener('click', function (event) {
 
     if (localStorage.getItem("Index") !== null)
         i = localStorage.getItem("Index");
 
     var selectedButton = event.target;
-    if (selectedButton.tagName != 'BUTTON') 
-    return;
+    if (selectedButton.tagName != 'BUTTON')
+        return;
 
     city = document.getElementById("searchInput").value;
     var displayCity = document.getElementById("cityName");
     displayCity.textContent = city + " (" + date + ")";
 
-    localStorage.setItem("History"+[i], city);
+    localStorage.setItem("History" + [i], city);
 
-    if(i < 7){
+    if (i < 7) {
         i++;
     }
     else {
